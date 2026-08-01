391a3473d9de2b169643e8f79beb89c8d2dee666
Homer Gaines <xirclebox@gmail.com>
2026-08-01 16:46:03 -0400

    more testing required

--- play.js
+++ play.js
@@ -53,5 +53,5 @@ document.addEventListener("click", (event) => {
 document.addEventListener("dragstart", (event) => {
   const link = bookmarkletFrom(event);
   if (!link) return;
-  track("a[data-bookmarklet]", { tool: link.dataset.bookmarklet });
+  track(a[data-bookmarklet], { tool: link.dataset.bookmarklet });
 });
